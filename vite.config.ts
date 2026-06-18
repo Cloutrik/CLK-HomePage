3e1fbf021cbabff99ebd54908ec86301f04eb868
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2026-06-18 00:40:33 -0300

    CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : "/",
+  base: "/",
   define: {
     global: "globalThis"
   },
